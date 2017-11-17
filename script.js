d75adf36bd0cb14f92fb83e3485ac2b96cf0a7ca
dylan delancie <dylandelancie@students.berkeley.net>
2017-11-17 11:38:17 -0800

    WOrk

---
+++ script.js
@@ -0,0 +1,11 @@
+function onSubmit(){
+
+}
+var guesses = 10;
+var words = ["bee","ball","united","france","jacket","berkeley","champion"];
+var rand = words[Math.floor(Math.random() * words.length)];
+var guessedLetters = [];
+
+
+
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
